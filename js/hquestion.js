00bee2d2bc6c9105fa033ec63d16cc28521deece
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2016-07-29 05:51:12 +0000

    first commit

---
+++ js/hquestion.js
@@ -0,0 +1,24 @@
+function question1() {
+    target = document.getElementById("output1");
+    target.innerHTML = "&lth1>と&lt/h1>の間にはさまっている文字は太字になるよ。h1やh2など数字は１～6までが使えるよ。";
+    }
+
+function question2() {
+    target = document.getElementById("output2");
+    target.innerHTML = "&ltb>と&lt/b>の間にはさまっている文字は太字になるよ";
+    }
+
+function question3() {
+    target = document.getElementById("output3");
+    target.innerHTML = "&ltu>と&lt/u>の間にはさまっている文字は太字になるよ。";
+    }
+
+function question4() {
+    target = document.getElementById("output4");
+    target.innerHTML = "&ltbr>書いた位置で改行できるよ。&ltbr>のときは終了タグはいりません♪";
+    }
+    
+function question5() {
+    target = document.getElementById("output5");
+    target.innerHTML = "&ltp>と&lt/p>の間にはさまっている部分が一つの段落になるよ♪<br>&ltp>と&lt/p>のペアを三つできるように、文を区切ろう！";
+    }
